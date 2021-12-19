5b8633c0726edf9cd53492ec0a2df5fe1c14b201
Starkmeng <58220799+Starkmeng@users.noreply.github.com>
2021-12-19 20:50:06 +0800

    vue  test01

---
+++ origin-monitor-Web/src/components/Test01.vue
@@ -0,0 +1,39 @@
+<template >
+  <div>
+    <div v-bind:class="isRed?'red':'pink'"></div>
+    <div v-bind:class="{red:true,big:true}"> 123456</div>
+    <ul>
+      <li v-for="stu in stus" v-bind:key="stu" :class="{'A':'red','B':'pink'}[stu.score]">
+        {{stu.name}}
+      </li>
+    </ul>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'Test01',
+  data () {
+    return {
+      isRed: true,
+      stus: [{name: 'mj', score: 'A'}, {name: 'psw', score: 'B'}]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red
+{
+  background-color: red;
+}
+.pink
+{
+   background-color: pink;
+ }
+.big
+{
+
+  font-size: 25px;
+}
+</style>
--- origin-monitor-Web/src/router/index.js
+++ origin-monitor-Web/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test01 from '../components/Test01'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Test01',
+      component: Test01
     }
   ]
 })
